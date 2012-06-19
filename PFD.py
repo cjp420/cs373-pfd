0da5d3636998077af81ff12efe2b27acd13b3fe6
Christian <christian.perez@utexas.edu>
2012-06-19 17:44:40 -0500

    finished the solve and wrote the print method and ready to test

--- PFD.py
+++ PFD.py
@@ -3,13 +3,15 @@
 
 class VertexPFD:
     def __init__(self, b) :
+	self.done = False
 	self.id = b[0]
 	self.pred = b[1]
-	self.preds = [0] * b[1]
+	self.preds = []
 	self.succ = []
 	i = 2
 	while i < len(b) :
-	    self.preds[i - 2] = b[i] 
+	    self.preds.append (b[i]) 
+	    i += 1
 
 
 # ------------
@@ -61,7 +63,7 @@ def pfd_read_rest (r, b) :
     l = s.split()
     i = 0
     while i < len(l) :
-	b[i] = int(l[i])
+	b.append (int(l[i]))
 	i += 1
     for j in b :
 	assert j > -1 and j <101
@@ -74,10 +76,30 @@ def pfd_read_rest (r, b) :
 def pfd_eval (vs) :
     """
     solves the tasks for a permutation that is a valid order of the tasks
-    vs is the list of VertexPFD with the index equal to the vertex number
+    vs is the dict of VertexPFD with the key equal to the vertex number
     """ 
-    z = []
-    for i in vs :
+    assert len (vs) > 0
+    r = []
+    low = 0
+    while  True:
+        z = []
+        for i in vs.keys() :
+	    if vs[i].pred == 0 and not vs[i].done :
+	        z.append (vs[i].id)
+        if len (z) == 0 :
+            break
+        low = 105
+        j = 0
+        while j < len (z) :
+	    if z[j] < low :
+	        low = z[j]
+	    j += 1
+        r.append (low)
+        vs[low].done = True
+        for k in vs[low].succ :
+	    vs[k].pred -= 1
+    assert len (r) > 0
+    return r
 	  
 
 
@@ -85,13 +107,16 @@ def pfd_eval (vs) :
 # pfd_print
 # -------------
 
-def pfd_print (r) :
+def pfd_print (w, re) :
     """
     prints the resulting permutation that is passed in
     r is the list of ints that holds the result
-    return
     """   
-       
+    s = ""
+    for i in re :
+	s += (str (i) + " ")
+    s += "\n"
+    w.write (s)
 
 # -------------
 # pfd_solve
@@ -99,23 +124,34 @@ def pfd_print (r) :
 
 def pfd_solve (r, w) :
     """
-    read, eval, print loop
+    read, eval, print 
     r is a reader
     w is a writer
     """
     a = [0, 0]
-    b = [0] * 100
     pfd_read_first(r, a)
-    c = [0] * a[0]
-    vs = [None]
+    b = []
+    vs = dict()
+    i = 1
+    while i <= a[0] :
+        vs[i] = VertexPFD([i, 0])
+	i += 1
     while pfd_read_rest(r, b) :
-        v = VertexPFD(b)
-	vs.append(v)
-    vs.sort(compare)
-    for i in vs :
-	if i.pred != 0 :
-	    for j in i.preds :
-		vs[j].succ.append (i.id)
-    r = pfd_solve (vs)
+        vs[b[0]].pred = b[1]
+	print b[0]
+	print (vs[b[0]].pred)
+        k = 2
+	while k < len(b) :
+	    vs[b[0]].preds.append (b[k])
+	    print (str (vs[b[0]].preds)) 
+	    k += 1
+        del b[:]
+    for i in vs.keys() :
+	if vs[i].pred != 0 :
+	    for j in vs[i].preds :
+		vs[j].succ.append (vs[i].id)
+		print (str (vs[j].succ) )
+    re = pfd_eval (vs)
+    pfd_print (w, re)
 
 	
